90b5782d131ff870915befa56a47b7f693b3078a
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-12-07 02:16:24 +0100

    Completed CartScreen
    ToDo: back button component, vytvořit dish component({className})

--- deliverooClone/navigation/navigation.js
+++ deliverooClone/navigation/navigation.js
@@ -1,12 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
-import { View, Text } from 'react-native';
 import React from 'react';
 
 import HomeScreen from '../screens/HomeScreen';
 import RestaurantScreen from '../screens/RestaurantScreen';
 import CartScreen from '../screens/CartScreen';
+import OrderPreparingScreen from '../screens/OrderPreparingScreen';
 
 export default function Navigation() {
   return (
@@ -17,6 +17,7 @@ export default function Navigation() {
             <Stack.Screen name='Home' component={HomeScreen}/>
             <Stack.Screen name='Restaurant' component={RestaurantScreen}/>
             <Stack.Screen name='Cart' component={CartScreen}/>
+            <Stack.Screen name="OrderPreparing" options={{presentation: "fullScreenModal"}} component={OrderPreparingScreen}/>
         </Stack.Navigator>
     </NavigationContainer>
   )
